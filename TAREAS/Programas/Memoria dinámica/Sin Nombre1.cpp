37b22b55689206647f9850b30b340e7d5e26538b
LABCONTROL\421-15 <422109899@pcpuma.acatlan.unam.mx>
2022-08-22 08:51:01 -0500

    programa memoria dinámica

---
+++ TAREAS/Programas/Memoria dinámica/Sin Nombre1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int *iptr;
+	
+	iptr = (int *)malloc(sizeof(int));
+	
+	if (iptr == NULL)
+	return -1;
+	
+	*iptr = 5;
+	
+	printf("%d, %p", *iptr, iptr);
+	
+	free(iptr);
+	
+	return 0;
+	
+	
+}
